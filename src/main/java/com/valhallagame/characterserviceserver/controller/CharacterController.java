480826fc4ef53d156f749f7835fad605fdb9c9fa
Mikael Berglund <mikbe@kth.se>
2017-11-24 22:11:44 +0100

    NO, dont include stupid stuff

--- src/main/java/com/valhallagame/characterserviceserver/controller/CharacterController.java
+++ src/main/java/com/valhallagame/characterserviceserver/controller/CharacterController.java
@@ -2,7 +2,6 @@ package com.valhallagame.characterserviceserver.controller;
 
 import java.util.Optional;
 
-import org.assertj.core.internal.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
